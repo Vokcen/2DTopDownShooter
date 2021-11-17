841ac15ff47c4443fcea556107798cb9c064f1dc
Vokcen <Karragaming@gmail.com>
2021-11-17 21:32:44 +0300

    ReferanceSystemAdded
    Referance system add.
    Referance system just keep them objects private and still usable without public.

---
+++ Assets/Scripts/System/DamagePopUp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DamagePopUp : MonoBehaviour
+{
+ 
+
+      
+    private TextMeshPro textMesh;
+ 
+
+     private void Awake() 
+     {
+        textMesh=transform.GetComponent<TextMeshPro>();
+    }
+    void  Setup(int damageAmount)
+    {
+             textMesh.SetText(damageAmount.ToString());
+    }
+     void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/System/ReferanceSystem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class  ReferanceSystem : MonoBehaviour
+{   
+    private static ReferanceSystem _r;
+    public static ReferanceSystem r{
+        get{    
+            if(_r==null)_r=(Instantiate(Resources.Load("ReferanceSystem"))as GameObject).GetComponent<ReferanceSystem>();
+           return _r;
+        }
+    }
+
+    
+    
+    }
+    
+
